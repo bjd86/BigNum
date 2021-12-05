d2fe37af630f837ebd9047e2ee1b67b5c7d63c59
Brian <bjd86@cornell.edu>
2021-12-05 02:32:56 -0500

    Fixed tests to run based on bignum calculator online. more accurate

--- test_gen.py
+++ test_gen.py
@@ -2,155 +2,171 @@ import random
 import re
 import subprocess
 import sys
+import requests
 
 import numpy as np
-from BigNumber import BigNumber
 
-NUMBER_SIZE = 30
-NUM_TESTS = 1
+class bcolors:
+  OK = '\033[92m' #GREEN
+  WARNING = '\033[93m' #YELLOW
+  FAIL = '\033[91m' #RED
+  RESET = '\033[0m' #RESET COLOR
+
+def PASS(message):
+  print(f"{bcolors.OK}{message}{bcolors.RESET}")
+
+def FAIL(message):
+  print(f"{bcolors.FAIL}{message}{bcolors.RESET}")
+
+
+NUM_TESTS = 100
 
 VERBOSE = 1
 
-def bignum_to_str(my_bignum : BigNumber.BigNumber) -> str:
-  """return string representation of BigNumber with
-  appropriate number of significant figures"""
-  return BigNumber.nstr(BigNumber.to_mpf(my_bignum), NUMBER_SIZE*2)
+operation_dict = {0 : "plus", 1 : "minus"}
 
-def sequence_check(bignum0 : BigNumber.BigNumber, bignum1 : BigNumber.BigNumber, num_digits : int) -> bool:
-  bignum0_str = bignum_to_str(bignum0)
-  bignum1_str = bignum_to_str(bignum1)
-  if( len(bignum0_str) < num_digits ):
-    bignum0_str = (len(num_digits - bignum0_str))*"0" + bignum0_str
+def sequence_check(bignum0 : str, bignum1 : str, num_digits : int) -> bool:
+  if( bignum0[0] == "-" ):
+    bignum0_sign = "-"
+    bignum0_num  = bignum0[1:]
   else:
-    bignum0_str = bignum0_str[len(bignum0_str) - num_digits:]
+    bignum0_sign = "+"
+    bignum0_num  = bignum0
 
-  if( len(bignum1_str) < num_digits ):
-    bignum1_str = (len(num_digits - bignum1_str))*"0" + bignum1_str
+  if( bignum1[0] == "-" ):
+    bignum1_sign = "-"
+    bignum1_num  = bignum1[1:]
   else:
-    bignum1_str = bignum1_str[len(bignum1_str) - num_digits:]
+    bignum1_sign = "+"
+    bignum1_num  = bignum1
+
+  if( len(bignum0_num) < num_digits ):
+    bignum0_num = (num_digits - len(bignum0_num))*"0" + bignum0_num
+  else:
+    bignum0_num = bignum0_num[len(bignum0_num) - num_digits:]
+
+  if( len(bignum1_num) < num_digits ):
+    bignum1_num = (num_digits - len(bignum1_num))*"0" + bignum1_num
+  else:
+    bignum1_num = bignum1_num[len(bignum1_num) - num_digits:]
 
   
   result = True
 
   for i in range(num_digits):
-    if( bignum0_str[i] != bignum1_str[i] ):
+    if( bignum0_num[i] != bignum1_num[i] ):
       result = False
 
-  print(f"cpp_BigNum: {bignum0_str} bignum_result: {bignum1_str}")
+  if( bignum0_sign != bignum1_sign ):
+    result = False
 
-  return result
-
-def sequence_check2(bignum0 : str, bignum1 : BigNumber.BigNumber, num_digits : int) -> bool:
-  # bignum0_str = bignum_to_str(bignum0)
-  bignum0_str = str(bignum0)
-  bignum1_str = bignum_to_str(bignum1)
-  if( len(bignum0_str) < num_digits ):
-    bignum0_str = (len(num_digits - bignum0_str))*"0" + bignum0_str
-  else:
-    bignum0_str = bignum0_str[len(bignum0_str) - num_digits:]
-
-  if( len(bignum1_str) < num_digits ):
-    bignum1_str = (len(num_digits - bignum1_str))*"0" + bignum1_str
-  else:
-    bignum1_str = bignum1_str[len(bignum1_str) - num_digits:]
-
-  
-  result = True
-
-  for i in range(num_digits):
-    if( bignum0_str[i] != bignum1_str[i] ):
-      result = False
-
-  print(f"cpp_BigNum: {bignum0_str} bignum_result: {bignum1_str}")
+  print(f"cpp_BigNum: {bignum0} bignum_result: {bignum1}")
 
   return result
 
 
-method = int(sys.argv[1])
-operation = int(sys.argv[2])
+def get_result_web(num0 : str, num1 : str, operation : str) -> str:
+  response = requests.get(f"https://www.calculator.net/big-number-calculator.html?cx={num0}&cy={num1}&cp=20&co={operation}")
 
-print("")
+  pattern = re.compile(".*Result.*break-word;\">(.*)</p><br><form name=\"calc\">.*")
+  result = pattern.search(response.text)
 
-# update_cmd = "make LL AR"
-update_cmd = "make AR"
+  result_w_comma = result.group(1)
 
-p = subprocess.run(update_cmd, shell=True)
+  return re.sub(",", "", result_w_comma)
 
-if( p.returncode ):
-  print("make failed")
-  sys.exit(1)
+if( __name__ == '__main__' ):
+  method = int(sys.argv[1])
+  operation_in = int(sys.argv[2])
 
-test_file = './test_nums.txt'
+  operation = operation_dict[operation_in]
 
-# seed with the current system time
-random.seed()
+  print("")
 
-num0 = []
-num1 = []
+  # update_cmd = "make LL AR"
+  update_cmd = "make AR"
 
-total_failed = 0
+  p = subprocess.run(update_cmd, shell=True)
 
-with open( test_file, 'w+' ) as f:
-  for j in range(NUM_TESTS):
-    num0 = []
-    num1 = []
-    num0_str = ""
-    num1_str = ""
-    for i in range(0, NUMBER_SIZE, 1):
-      num0_digit = random.randint(0, 9)
-      num0.append(num0_digit)
-      num0_str += str(num0_digit)
-      num1_digit = random.randint(0, 9)
-      num1.append(num1_digit)
-      num1_str += str(num1_digit)
+  if( p.returncode ):
+    print("make failed")
+    sys.exit(1)
 
-    bignum0 = BigNumber.BigNumber(num0_str)
-    bignum1 = BigNumber.BigNumber(num1_str)
+  test_file = './test_nums.txt'
 
-    if( operation ):
-      print(f"testing {num0_str} - {num1_str}")
-    else:
-      print(f"testing {num0_str} + {num1_str}")
+  # seed with the current system time
+  random.seed()
 
-    bignum_result = bignum0 + bignum1
+  num0 = []
+  num1 = []
 
-    # result_test2 = bignum0 + bignum1 + BigNumber.BigNumber("1")
-    # print(f"test: {result_test2.value == bignum_result.value}")
+  total_failed = 0
 
-    if( method == 0 ):
-      if( operation ):
-        test_add = f"./AR {num0_str} {num1_str} {NUMBER_SIZE} 1"
+  with open( test_file, 'w+' ) as f:
+    for j in range(NUM_TESTS):
+      num0 = []
+      num1 = []
+      num0_str = ""
+      num1_str = ""
+      NUMBER_SIZE = random.randint(10,100)
+      for i in range(0, NUMBER_SIZE, 1):
+        num0_digit = random.randint(0, 9)
+        num0.append(num0_digit)
+        num0_str += str(num0_digit)
+        num1_digit = random.randint(0, 9)
+        num1.append(num1_digit)
+        num1_str += str(num1_digit)
+
+      bignum0 = num0_str
+      bignum1 = num1_str
+
+      if( operation == "minus" ):
+        print(f"testing {num0_str} - {num1_str}")
       else:
-        test_add = f"./AR {num0_str} {num1_str} {NUMBER_SIZE} 0"
-    else:
-      if( operation ):
-        test_add = f"./LL {num0_str} {num1_str} {NUMBER_SIZE} 1"
+        print(f"testing {num0_str} + {num1_str}")
+
+      bignum_result = get_result_web(num0_str, num1_str, operation)
+
+      # result_test2 = bignum0 + bignum1 + BigNumber.BigNumber("1")
+      # print(f"test: {result_test2.value == bignum_result.value}")
+
+      if( method == 0 ):
+        if( operation == "minus" ):
+          test_add = f"./AR {num0_str} {num1_str} {NUMBER_SIZE} 1"
+        else:
+          test_add = f"./AR {num0_str} {num1_str} {NUMBER_SIZE} 0"
       else:
-        test_add = f"./LL {num0_str} {num1_str} {NUMBER_SIZE} 0"
+        if( operation == "minus" ):
+          test_add = f"./LL {num0_str} {num1_str} {NUMBER_SIZE} 1"
+        else:
+          test_add = f"./LL {num0_str} {num1_str} {NUMBER_SIZE} 0"
 
-    if( VERBOSE ):
-      print(f"running command: {test_add}\n")
+      if( VERBOSE ):
+        print(f"running command: {test_add}\n")
 
-    output = subprocess.run(test_add, capture_output=True, shell=True)
+      output = subprocess.run(test_add, capture_output=True, shell=True)
 
-    # print(f"output: {output.stdout}")
+      # print(f"output: {output.stdout}")
 
-    cpp_result = re.findall("(-?\d+)", str(output.stdout))[0]
+      cpp_result = re.findall("(-?\d+)", str(output.stdout))[0]
 
-    if( VERBOSE ):
-      print(f"python result : {bignum_to_str(bignum_result)}")
-      print(f"returned value: {cpp_result}")
+      if( VERBOSE ):
+        print(f"python result : {bignum_result}")
+        print(f"returned value: {cpp_result}")
 
-    # trying to avoid using python BigNum because it seems to suck
-    if( sequence_check2(cpp_result, bignum_result, NUMBER_SIZE) ):
-      print("Pass")
-    else:
-      print("Fail")
-      total_failed += 1
+      # trying to avoid using python BigNum because it seems to suck
+      if( sequence_check(cpp_result, bignum_result, NUMBER_SIZE) ):
+        PASS("Pass")
+      else:
+        FAIL("Fail")
+        total_failed += 1
+        f.write(f"\n{num0_str} {operation} {num1_str} = {cpp_result}\n")
+        f.write(f"correct answer: {bignum_result}\n")
 
-print("\nRESULTS:")
 
-if( total_failed ):
-  print(f"Failed {total_failed} out of {NUM_TESTS} tests with number size {NUMBER_SIZE}")
-else:
-  print(f"Passed {NUM_TESTS} tests with number size {NUMBER_SIZE}")
+  print("\nRESULTS:")
+
+  if( total_failed ):
+    FAIL(f"{bcolors.FAIL}Failed{bcolors.RESET} {total_failed} out of {NUM_TESTS} tests with number size {NUMBER_SIZE}")
+  else:
+    PASS(f"{bcolors.OK}Passed{bcolors.RESET} {NUM_TESTS} tests with number size {NUMBER_SIZE}")
